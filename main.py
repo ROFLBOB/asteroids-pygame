0900451ec93e5b3da2910520c1b13a0f6eafc662
kingpoogie <bmadeley@getgoldn.com>
2024-11-10 13:23:53 -0500

    pygame running

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 #this allows us to use code from the open-source pygame lihbrary throughout this file
 import pygame
+import constants
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        #fill the screen blue
+        screen.fill((0,0,255))
+
+        #update the screen
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
