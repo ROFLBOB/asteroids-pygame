cfc590c2051c5188994e9c1988195f9dd03ccd34
kingpoogie <bmadeley@getgoldn.com>
2024-11-11 12:35:10 -0500

    player rotation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #this allows us to use code from the open-source pygame lihbrary throughout this file
 import pygame
 import constants
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -9,16 +10,26 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    timer = pygame.time.Clock()
+    dt = 0
+    player = Player(x = constants.SCREEN_WIDTH/2, y = constants.SCREEN_HEIGHT/2)
 
     while True:
+        #get the quit button working
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         #fill the screen blue
         screen.fill((0,0,255))
+        player.draw(screen)
+
+        #update the player
+        player.update(dt)
 
         #update the screen
         pygame.display.flip()
+        dt = timer.tick(60)/1000
 
 if __name__ == "__main__":
     main()
